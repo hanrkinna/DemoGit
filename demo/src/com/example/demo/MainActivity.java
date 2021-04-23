436b22436e57613c708b1c35230157b83accb73d
hanrkinna <lujianwei2wdk@126.com>
2021-04-23 18:35:23 +0800

    特特

--- demo/src/com/example/demo/MainActivity.java
+++ demo/src/com/example/demo/MainActivity.java
@@ -16,7 +16,6 @@ public class MainActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		System.out.println("冲突");
 	}
 
 	class MultiTouchView extends View {
