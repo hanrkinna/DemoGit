ac23d1d562276b953cb8d50cf382aa205af2cd7e
hanrkinna <lujianwei2wdk@126.com>
2021-04-26 11:35:12 +0800

    syso

--- demo/src/com/example/demo/MainActivity.java
+++ demo/src/com/example/demo/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		System.out.println("11373");
 	}
 
 	class MultiTouchView extends View {
