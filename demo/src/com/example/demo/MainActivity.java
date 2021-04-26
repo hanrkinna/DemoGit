211abe0a8b9d0810f81b649ea86b3ca4827901c5
hanrkinna <lujianwei2wdk@126.com>
2021-04-26 14:28:40 +0800

    remove two a.txt

--- demo/src/com/example/demo/MainActivity.java
+++ demo/src/com/example/demo/MainActivity.java
@@ -15,10 +15,7 @@ public class MainActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		  System.out.println(11);
-		  if(true){
-			  System.out.println("我是测试的！！！！");
-		  }
+		
 	}
 
 	class MultiTouchView extends View {
