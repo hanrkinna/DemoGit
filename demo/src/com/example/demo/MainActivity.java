b43d166c0ab48c5100d7742bd2c231a9c1850a1f
hanrkinna <lujianwei2wdk@126.com>
2021-04-26 14:59:08 +0800

    test3

--- demo/src/com/example/demo/MainActivity.java
+++ demo/src/com/example/demo/MainActivity.java
@@ -16,11 +16,12 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
           System.out.println(11);
-          if(true){
+         
               System.out.println("我是测试的！！！！");
               System.out.println("我是冲突！！！");
               System.out.println("你是你妹");
-          }
+              
+          
     }
 
     class MultiTouchView extends View {
