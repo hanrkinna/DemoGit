713a95d5a518acf04990e682a55e83c37d6b1261
hanrkinna <lujianwei2wdk@126.com>
2021-04-26 14:36:58 +0800

    chongtu jiejue

--- demo/src/com/example/demo/MainActivity.java
+++ demo/src/com/example/demo/MainActivity.java
@@ -12,69 +12,70 @@ import android.view.View;
 
 public class MainActivity extends Activity {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		  System.out.println(11);
-		  if(true){
-			  System.out.println("我是测试的！！！！");
-			  System.out.println("你是你妹");
-		  }
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+          System.out.println(11);
+          if(true){
+              System.out.println("我是测试的！！！！");
+              System.out.println("我是冲突！！！");
+              System.out.println("你是你妹");
+          }
+    }
 
-	class MultiTouchView extends View {
+    class MultiTouchView extends View {
 
-		private float x1;
-		private float y1;
-		private float x2;
-		private float y2;
+        private float x1;
+        private float y1;
+        private float x2;
+        private float y2;
 
-		public MultiTouchView(Context context) {
-			super(context);
+        public MultiTouchView(Context context) {
+            super(context);
 
 
-		}
+        }
 
-		@Override
-		public boolean onTouchEvent(MotionEvent event) {
-			System.out.print("测试冲突");
-			float size = event.getSize();
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            System.out.print("测试冲突");
+            float size = event.getSize();
 
-			int szi = (int) size;
-			int dxi = szi >> 12;
-			int dyit = ((1 << 12) - 1);
-			int dyi = szi & dyit;
+            int szi = (int) size;
+            int dxi = szi >> 12;
+            int dyit = ((1 << 12) - 1);
+            int dyi = szi & dyit;
 
-			DisplayMetrics metrics = getResources().getDisplayMetrics();
-			float dx = metrics.widthPixels * dxi / (float) dyit;
-			float dy = metrics.heightPixels * dyi / (float) dyit;
+            DisplayMetrics metrics = getResources().getDisplayMetrics();
+            float dx = metrics.widthPixels * dxi / (float) dyit;
+            float dy = metrics.heightPixels * dyi / (float) dyit;
 
-			x1 = event.getX();
-			y1 = event.getY();
+            x1 = event.getX();
+            y1 = event.getY();
 
-			x2 = x1 + dx;
-			y2 = y1 + dy;
+            x2 = x1 + dx;
+            y2 = y1 + dy;
 
-			invalidate();
+            invalidate();
 
-			return true;
-		}
+            return true;
+        }
 
-		@Override
-		protected void onDraw(Canvas canvas) {
-			// TODO Auto-generated method stub
-			super.onDraw(canvas);
+        @Override
+        protected void onDraw(Canvas canvas) {
+            // TODO Auto-generated method stub
+            super.onDraw(canvas);
 
-			float r = (float) Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2)
-					* (y1 - y2)) / 2;
-			r = 50 >= r ? 50 : r;
+            float r = (float) Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2)
+                    * (y1 - y2)) / 2;
+            r = 50 >= r ? 50 : r;
 
-			Paint paint = new Paint();
-			paint.setColor(Color.BLUE);
-			canvas.drawCircle(x1, y1, r, paint);
+            Paint paint = new Paint();
+            paint.setColor(Color.BLUE);
+            canvas.drawCircle(x1, y1, r, paint);
 
-		}
+        }
 
-	}
+    }
 
 }
